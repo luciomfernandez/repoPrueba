57d3dad7f44ecbb8a2aaa65178caba2576051210
Valeria Ancora <Valeria Ancora@192.168.1.108>
2018-05-28 23:28:55 -0300

    primer commit con push

--- PruebaGit/src/clasePrincipal.java
+++ PruebaGit/src/clasePrincipal.java
@@ -4,7 +4,7 @@ public class clasePrincipal {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("PruebaGit");
-		System.out.println("Agrego");
+		System.out.println("Agrego1");
 	}
 
 }
