f6c31d7380d91d194bd4c5f942e5ce0f270df1ae
Lucio <Lucio Fernandez@192.168.1.108>
2018-05-28 23:42:19 -0300

    dsd

--- PruebaGit/src/clasePrincipal.java
+++ PruebaGit/src/clasePrincipal.java
@@ -4,9 +4,9 @@ public class clasePrincipal {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("PruebaGit");
-		System.out.println("Agrego2");
-		System.out.println("Agrego3");
 		
+		System.out.println("Agrego3");
+		System.out.println("Agrego4");
 	}
 
 }
