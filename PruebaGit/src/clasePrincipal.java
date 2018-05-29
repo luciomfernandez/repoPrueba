ff1698453d792d58566060ed5a0dc37600e89a57
Valeria Ancora <Valeria Ancora@192.168.1.108>
2018-05-28 23:31:27 -0300

    sssss

--- PruebaGit/src/clasePrincipal.java
+++ PruebaGit/src/clasePrincipal.java
@@ -4,7 +4,7 @@ public class clasePrincipal {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("PruebaGit");
-		System.out.println("Agrego1");
+		System.out.println("Agrego2");
 	}
 
 }
